0b0be0af503d3026feee18c14348cade5f4009cd
Hoplan <104734909+Hxplan@users.noreply.github.com>
2025-01-03 21:12:42 +0100

    Update HoplanHub.lua

--- HoplanHub.lua
+++ HoplanHub.lua
@@ -9,14 +9,6 @@ local GUI = Mercury:Create{
     Link = "https://github.com/deeeity/mercury-lib"
 }
 
-
-GUI:Credit{
-	Name = "Hoplan",
-	Description = "Made the HUB",
-	V3rm = "discord.gg/senritsu",
-	Discord = "Hoplan_"
-}
-
 -- Tabs creation
 local Tab = GUI:Tab{
     Name = "Collect All Pets!",
